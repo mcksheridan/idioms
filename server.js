bf256d21756c6a6e15aa59dec396be66c1cde3ed
Sheridan McKisick <mcksheridan@gmail.com>
2021-04-05 22:34:18 -0500

    Create server side function to get weather data using the latitude and longitude passed in as queries. A call to this endpoint can be made from the client side. This way, the API key is not exposed.

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 import express from 'express';
 import path from 'path';
+import fetch from 'node-fetch';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const app = express();
 const PORT = 3000;
@@ -8,6 +12,27 @@ app.use(express.static(path.join(__dirname, 'src')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'src/html/index.html'));
+});
+
+app.get('/weather', (req, res) => {
+  const latitude = req.query.lat;
+  const longitude = req.query.lon;
+  const getWeatherData = async () => {
+    if (latitude && longitude) {
+      const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${process.env.API_KEY}`);
+      const data = await response.json();
+      if (!data) {
+        res.send('Error');
+      }
+      if (data) {
+        res.json(data);
+      }
+    }
+    if (!latitude || !longitude) {
+      res.json('Enter latitude and longitude');
+    }
+  };
+  getWeatherData();
 });
 
 app.listen(process.env.PORT || PORT);
